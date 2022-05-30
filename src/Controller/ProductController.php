0c93371df183da4d195734c377e348233476a911
Alexander Fischer <fischer.online@gmx.net>
2022-05-30 19:54:29 +0200

    Persisting Objects to the Database

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @param ManagerRegistry $doctrine
+     * @return Response
+     * @Route("/product", name="create_product")
+     */
+    public function createProduct(ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+
+        $entityManager->persist($product);
+
+        $entityManager->flush();
+
+        return new Response('Saved new product with id ' .$product->getId());
+    }
+
+    /**
+     * @Route("/product", name="app_product")
+     */
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}
